7a4a9308c17599004123921e5aef419457255519
Diego ROYER <diroyer@e3r10p16.clusters.42paris.fr>
2022-10-24 18:37:44 +0200

    mal de crane

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,12 +6,13 @@
 /*   By: diroyer <diroyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/22 12:16:29 by diroyer           #+#    #+#             */
-/*   Updated: 2022/10/24 17:47:38 by diroyer          ###   ########.fr       */
+/*   Updated: 2022/10/24 18:36:31 by diroyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 #define LONG_MAX 0x7fffffffffffffff
+#define LONG_MIN 0x8000000000000000
 
 void	exit_numeric(char *str)
 {
@@ -38,44 +39,86 @@ int	ft_isnum(char *str)
 	return (1);
 }
 
-static unsigned long ft_atoi2(const char *nptr)
+long	ft_atoi2(const char *nptr)
 {
 	long		result;
-	long					sign;
-	size_t				i;
-	int over; 
+	int		sign;
+	size_t	i;
+	int over;
 
-	over = 0;
 	i = 0;
+	over = 0;
 	result = 0;
-	sign = 1;
 	while ((nptr[i] >= 9 && nptr[i] <= 13) || nptr[i] == ' ')
 		i++;
-	if (nptr[i] == '+' || nptr[i] == '-')
-	{
-		if (nptr[i] == '-')
-			sign = -1;
-		i++;
-	}
+	sign = 1 + ((nptr[i] == '-' && ++i) * -2);
+	printf("sign = %d\n", sign);
 	while (nptr[i] >= '0' && nptr[i] <= '9')
 	{
 		if (result != (result * 10) / 10)
 			over = 1;
+		if (result > 0 && ((result * 10) + (nptr[i]) - '0') == 0)
+			over = 1;
 		result = result * 10 + (nptr[i++] - '0');
+		printf("over = %d result = %ld\n", over, result);
 		if (result - (sign < 0) < 0)
 			over = 1;
+		
 	}
-	if (result > LONG_MAX || over == 1)
+	if ((result > LONG_MAX && sign == 1) || over == 1)
+	{
+		exit_numeric((char*)nptr);
+		return (2);
+	}
+	if ((long unsigned)result > LONG_MIN && sign == -1)
 	{
 		exit_numeric((char*)nptr);
 		return (2);
 	}
 	return (result * sign);
 }
+/*
+   static int	ft_atoi2(const char *nptr)
+   {
+   long	result;
+   long					sign;
+   size_t				i;
+   int over; 
 
+   over = 0;
+   i = 0;
+   result = 0;
+   sign = 1;
+   while ((nptr[i] >= 9 && nptr[i] <= 13) || nptr[i] == ' ')
+   i++;
+   if (nptr[i] == '+' || nptr[i] == '-')
+   {
+   if (nptr[i] == '-')
+   sign = -1;
+   i++;
+   }
+   while (nptr[i] >= '0' && nptr[i] <= '9')
+   {
+   printf("result * 10 / 10 = %li\n", (result * 10) / 10);
+   if (result != (result * 10) / 10)
+   over = 1;
+   result = result * 10 + (nptr[i++] - '0');
+   printf(" result %li\n", result);
+   if (result - (sign < 0) < 0)
+   over = 1;
+   }
+   printf("over = %d\n", over);
+   if (result > LONG_MAX || over == 1)
+   {
+   exit_numeric((char*)nptr);
+   return (2);
+   }
+   return (result * sign);
+   }
+ */
 int	ft_exit(char *av)
 {
-	int	status;
+	long	status;
 
 	ft_putstr_fd("exit\n", 2);
 	if (!*av)
@@ -89,7 +132,7 @@ int	ft_exit(char *av)
 			status = status % 256;
 		status = status + 256;
 	}
-	if (status > 256)
+	if (status > 255)
 		status = status % 256;
 	return (status);
 }
