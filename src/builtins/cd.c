e7241864079d53e0c84e990d001c795768f5f567
Diego ROYER <diroyer@e3r12p22.clusters.42paris.fr>
2022-11-21 00:40:23 +0100

    cd -

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: diroyer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 14:34:03 by diroyer           #+#    #+#             */
-/*   Updated: 2022/11/20 21:17:54 by momadani         ###   ########.fr       */
+/*   Updated: 2022/11/21 00:40:14 by diroyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,55 +34,46 @@ static void	replace_pwd(char *replace, t_env *lst)
 	ft_replace_env(lst, replace, cwd);
 }
 
-static int	swap_pwd(char *wd, t_env *lst)
+static int	swap_old_pwd(char *wd, t_env *lst)
 {
-	static int	i = 0;
+	char 	*old;
 
 	if (ft_strcmp(get_key_value(lst, "PWD"), get_key_value(lst, "OLDPWD")))
 	{
-		if (i == 0)
+		old = ft_strdup(get_key_value(lst, "PWD"));
+		if (!chdir(get_key_value(lst, "OLDPWD")))
 		{
-			if (!chdir(wd))
-				replace_pwd("PWD", lst);
-			else
-				return (cd_error(wd));
-			i = 1;
+			replace_pwd("PWD", lst);
+			ft_replace_env(lst, "OLDPWD", old);
+			printf("%s\n", get_key_value(lst, "PWD"));
 		}
 		else
-		{
-			if (!chdir(wd))
-				replace_pwd("PWD", lst);
-			else
-				return (cd_error(wd));
-			i = 0;
-		}
+			return (cd_error(wd));
 	}
+	else
+		printf("%s\n", get_key_value(lst, "PWD"));
 	return (0);
 }
 
 int	ft_cd(t_env *lst, char **av, int ac)
 {
-	int		i;
-
-	i = 1;
-	while (ac > 1 && av[i][0] == '-' && ac--)
-		i++;
-	if (ac == 1 || ac == 2)
+	if (ac == 2 && !ft_strcmp(av[1], "-"))
+		return (swap_old_pwd(av[1], lst));
+	else if (ac == 1)
 	{
 		replace_pwd("OLDPWD", lst);
-		if (ac == 1 && chdir(get_key_value(lst, "HOME")))
+		if (!get_key_value(lst, "HOME") && chdir(get_key_value(lst, "HOME")))
 			return (ft_error("cd: HOME not set", NULL, NULL, 1));
 		else
 			ft_replace_env(lst, "PWD", ft_strdup(get_key_value(lst, "HOME")));
-		if (ac == 2)
-		{
-			if (!ft_strcmp(av[i], "-"))
-				return (swap_pwd(av[i], lst));
-			if (!chdir(av[i]))
-				replace_pwd("PWD", lst);
-			else
-				return (cd_error(av[i]));
-		}
+	}
+	else if (ac == 2)
+	{
+		replace_pwd("OLDPWD", lst);
+		if (!chdir(av[1]))
+			replace_pwd("PWD", lst);
+		else
+			return (cd_error(av[1]));
 	}
 	else
 		return (ft_error("cd : too many arguments", NULL, NULL, 1));
